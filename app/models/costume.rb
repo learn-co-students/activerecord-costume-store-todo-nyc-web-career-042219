8e9904a8b305af21574bed2d465b57c73db3d91f
Jason Gomez <jason.gomez@me.com>
2019-05-02 18:56:21 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: costumes
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :integer
+#  size       :string
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+  
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,19 @@
+# == Schema Information
+#
+# Table name: haunted_houses
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  location         :string
+#  theme            :string
+#  price            :integer
+#  family_friendly? :boolean
+#  opening_date     :datetime
+#  closing_date     :datetime
+#  long_descripton  :text
+#
+
 # Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Base
+  
+end
